2b1e2db2dfeb590d847b9e304810b9989adab5f0
PedroHCastilhos <pedro.castilhos.fraga@gmail.com>
2025-10-13 17:36:06 -0300

    Atualização dos métodos CRUD, importação do bcrypt e do jwt

--- MockBackend/src/user/user.module.ts
+++ MockBackend/src/user/user.module.ts
@@ -1,9 +1,19 @@
 import { Module } from '@nestjs/common';
 import { UserController } from './user.controller';
 import { UserService } from './user.service';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { JwtModule } from '@nestjs/jwt';
+import { jwtConstants } from './constants';
 
 @Module({
+  imports: [
+    JwtModule.register({
+      global: true,
+      secret: jwtConstants.secret,
+      signOptions: {expiresIn: '60s'}
+    }),
+  ],
   controllers: [UserController],
-  providers: [UserService]
+  providers: [UserService, PrismaService]
 })
 export class UserModule {}
